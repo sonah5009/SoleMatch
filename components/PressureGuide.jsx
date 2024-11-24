1a01ecf5222530089ffb1db44958240c62958329
sonah5009 <sonah5009@gmail.com>
2024-11-25 01:25:25 +0900

    Edit: PressureGuide.jsx component

--- components/PressureGuide.jsx
+++ components/PressureGuide.jsx
@@ -1,11 +1,65 @@
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 import React from "react";
+import { Colors } from "@/constants/Colors";
+import NavigateButton from "./NavigateButton";
+import ActionButton from "./ActionButton";
 
-export default function PressureGuide() {
+const window = Dimensions.get("window");
+
+export default function PressureGuide({
+  type,
+  bottomText,
+  buttonTitle,
+  buttonHandler,
+  buttonLink,
+}) {
   return (
-    <Image
-      source={require("../assets/images/pressure-both.png")}
-      style={{ height: 100 }}
-    />
+    <>
+      <View style={styles.main}>
+        {type === "ing" ? (
+          <ActivityIndicator size="large" color={Colors.green.green100} />
+        ) : (
+          <Image
+            source={require("../assets/images/pressure-both.png")}
+            style={styles.image}
+          />
+        )}
+      </View>
+      <View style={styles.bottom}>
+        <Text>{bottomText}</Text>
+        {type === "start" && (
+          <ActionButton onPress={buttonHandler} title={buttonTitle} />
+        )}
+        {type === "end" && (
+          <NavigateButton title={buttonTitle} link={buttonLink} />
+        )}
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    height: window.width > 500 ? window.height * 0.6 : window.height * 0.6,
+  },
+  image: {
+    width: "auto",
+    height: window.width > 500 ? window.height * 0.6 : window.height * 0.6,
+    resizeMode: "contain",
+    marginHorizontal: 50,
+  },
+  bottom: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "space-evenly",
+    paddingHorizontal: window.width > 500 ? 350 : 20,
+  },
+});
