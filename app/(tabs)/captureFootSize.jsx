0fbb0e525ebb627c784a55c8df10fd6813cfef36
sonah5009 <sonah5009@gmail.com>
2024-11-26 21:51:40 +0900

    edit: axios -> fetch & Array 추가

--- app/(tabs)/captureFootSize.jsx
+++ app/(tabs)/captureFootSize.jsx
@@ -12,18 +12,13 @@ import {
   Modal,
   ActivityIndicator,
 } from "react-native";
-import {
-  Camera,
-  CameraType,
-  CameraView,
-  useCameraPermissions,
-} from "expo-camera";
-import { Base64 } from 'react-native-base64';
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { Base64 } from "react-native-base64";
 
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from "@react-native-picker/picker";
 import * as FileSystem from "expo-file-system";
 import { MaterialIcons } from "@expo/vector-icons"; // 아이콘을 위해 추가
-import axios from 'axios';
+// import axios from "axios";
 
 const WINDOW_HEIGHT = Dimensions.get("window").height;
 const WINDOW_WIDTH = Dimensions.get("window").width;
@@ -34,7 +29,7 @@ export default function captureFootSize() {
 
   const [users, setUsers] = useState([]); // users state updated
   const [selectedUser, setSelectedUser] = useState(null);
-  
+
   const [leftfeet, setLeftfeet] = useState(false);
   const [rightfeet, setRightfeet] = useState(false);
 
@@ -46,24 +41,34 @@ export default function captureFootSize() {
   const [rightLength, setRightLength] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const cameraRef = useRef(null);
-  
+
   // const BASE_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
   const BASE_URL = process.env.EXPO_PUBLIC_BACKEND_LOCAL_URL;
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
+        // const response = await axios.get(`${BASE_URL}/users`);
+        // setUsers(response.data); // Updated to use response data directly
+
         console.log("Fetching users...");
-        const response = await axios.get(`${BASE_URL}/users`);
-        setUsers(response.data); // Updated to use response data directly
-        setSelectedUser(response.data[0]?.userName || null); // Set the first user or null
+        const response = await fetch(`${BASE_URL}/users`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // JSON 데이터 파싱
+        const data = await response.json();
+        console.log("Fetched users:", data);
+
+        setUsers(data); // Updated to use response data directly
+        setSelectedUser(data[0]?.userName || null); // Set the first user or null
       } catch (error) {
         console.error("Failed to fetch users:", error);
       }
     };
     fetchUsers();
   }, []);
-  const initialize = async() => {
+  const initialize = async () => {
     setLeftfeet(false);
     setRightfeet(false);
     setLeftResult(null);
@@ -73,7 +78,7 @@ export default function captureFootSize() {
     setRightWidth(null);
     setRightLength(null);
     setImageURI(null);
-  }
+  };
   const takeImage = async () => {
     if (cameraRef.current) {
       const photo = await cameraRef.current?.takePictureAsync();
@@ -86,7 +91,7 @@ export default function captureFootSize() {
     let filename;
     if (!leftfeet) {
       setLeftfeet(true);
-      filename=selectedUser+"_left.jpg";
+      filename = selectedUser + "_left.jpg";
     } else {
       setRightfeet(true);
       filename = selectedUser + "_right.jpg";
@@ -94,44 +99,55 @@ export default function captureFootSize() {
     const formData = new FormData();
     formData.append("file", {
       uri: photo,
-      type: 'image/jpeg',
-      name: filename
+      type: "image/jpeg",
+      name: filename,
     }); // The third parameter specifies the filename
     formData.append("fileName", filename); // The third parameter specifies the filename
     formData.append("user", selectedUser); // The third parameter specifies the filename
-    
-    if(rightfeet) {
+
+    if (rightfeet) {
       setLeftfeet(false);
       setRightfeet(false);
     }
-    if(!rightfeet){
+    if (!rightfeet) {
       setImageURI(null);
     }
     try {
-      const res = await axios.post(`${BASE_URL}/analyze_size`, formData, {
+      // POST 요청 실행
+      const response = await fetch(`${BASE_URL}/analyze_size`, {
+        method: "POST",
         headers: {
-          "Content-Type": "multipart/form-data",
+          // fetch에서는 FormData와 함께 Content-Type을 설정할 필요 없음
         },
+        body: formData,
       });
-      console.log("hii");
 
-      if (res.data && res.data.image) {
-        // If it's a base64 image string
+      if (!response.ok) {
+        console.log("업로드 되는 지 확인");
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      // 응답 데이터를 JSON으로 변환
+      const resData = await response.json();
+
+      // 응답 데이터 처리
+      if (resData && resData.image) {
+        // Base64 이미지 처리
         if (!leftResult) {
-          setLeftResult(`data:image/png;base64,${res.data.image}`);
-          setLeftWidth(res.data.width);
-          setLeftLength(res.data.length);
+          setLeftResult(`data:image/png;base64,${resData.image}`);
+          setLeftWidth(resData.width);
+          setLeftLength(resData.length);
         } else {
-          setRightResult(`data:image/png;base64,${res.data.image}`);
-          setRightWidth(res.data.width);
-          setRightLength(res.data.length);
+          setRightResult(`data:image/png;base64,${resData.image}`);
+          setRightWidth(resData.width);
+          setRightLength(resData.length);
         }
       } else {
         alert("다시 시도해주세요.");
-
       }
     } catch (error) {
-      console.error("Error uploading image:", error);
+      console.error("Failed to analyze size:", error);
+      alert("오류가 발생했습니다. 다시 시도해주세요.");
     } finally {
       setIsLoading(false); // 로딩 종료
     }
@@ -178,89 +194,102 @@ export default function captureFootSize() {
         </View>
       </Modal>
       {leftResult && rightResult ? (
-  <View style={styles.imageContainer}>
-    <View style={styles.middleContainerFirst}>
-      <Image
-        source={{ uri: leftResult }}
-        style={{
-          width: "50%",
-          resizeMode: "contain",
-        }}
-      />
-      <Image
-        source={{ uri: rightResult }}
-        style={{
-          width: "50%",
-          resizeMode: "contain",
-        }}
-      />
-    </View>
-    <View style={styles.middleContainerSecond}>
-      <View style={styles.imageText}>
-        <Text style={styles.imageTextTitle}>Left Foot</Text>
-        <Text>Width</Text>
-        <Text>{Math.round(leftWidth * 100)/100} mm</Text>
-        <Text>Length</Text>
-        <Text>{Math.round(leftLength * 100)/100} mm</Text>
-      </View>
-      <View style={styles.imageText}>
-        <Text style={styles.imageTextTitle}>Right Foot</Text>
-        <Text>Width</Text>
-        <Text>{Math.round(rightWidth * 100)/100} mm</Text>
-        <Text>Length</Text>
-        <Text>{Math.round(rightLength * 100)/100} mm</Text>
-      </View>
-    </View>
-    <TouchableOpacity style={styles.confirmButton} onPress={() => initialize()}>
-      <Text style={styles.confirmText}>Retry</Text>
-    </TouchableOpacity>
-  </View>
-) : imageURI ? (
-  <View style={styles.previewContainer}>
-    <Image source={{ uri: imageURI }} style={styles.preview} />
-    <View style={styles.buttonContainer2}>
-      <TouchableOpacity
-        style={styles.retakeButton}
-        onPress={() => setImageURI(null)}
-      >
-        <Text style={styles.retakeText}>Retake</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.retakeButton}
-        onPress={() => sendImageToServer(imageURI)}
-      >
-        <Text style={styles.confirmText}>Confirm</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-) : (
-  
-
-          <CameraView ref={cameraRef} style={styles.camera} ratio="16:9">
-            <View style={styles.pickerset}>
-              <Text style={styles.label}>사용자 선택</Text>
-              <Picker
-                selectedValue={selectedUser}
-                onValueChange={(itemValue) => setSelectedUser(itemValue)}
-                style={styles.picker}
-              >
-                {users.map((user, index) => (
-                  <Picker.Item key={index} label={user.userName} value={user.userName} />
+        <View style={styles.imageContainer}>
+          <View style={styles.middleContainerFirst}>
+            <Image
+              source={{ uri: leftResult }}
+              style={{
+                width: "50%",
+                resizeMode: "contain",
+              }}
+            />
+            <Image
+              source={{ uri: rightResult }}
+              style={{
+                width: "50%",
+                resizeMode: "contain",
+              }}
+            />
+          </View>
+          <View style={styles.middleContainerSecond}>
+            <View style={styles.imageText}>
+              <Text style={styles.imageTextTitle}>Left Foot</Text>
+              <Text>Width</Text>
+              <Text>{Math.round(leftWidth * 100) / 100} mm</Text>
+              <Text>Length</Text>
+              <Text>{Math.round(leftLength * 100) / 100} mm</Text>
+            </View>
+            <View style={styles.imageText}>
+              <Text style={styles.imageTextTitle}>Right Foot</Text>
+              <Text>Width</Text>
+              <Text>{Math.round(rightWidth * 100) / 100} mm</Text>
+              <Text>Length</Text>
+              <Text>{Math.round(rightLength * 100) / 100} mm</Text>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={styles.confirmButton}
+            onPress={() => initialize()}
+          >
+            <Text style={styles.confirmText}>Retry</Text>
+          </TouchableOpacity>
+        </View>
+      ) : imageURI ? (
+        <View style={styles.previewContainer}>
+          <Image source={{ uri: imageURI }} style={styles.preview} />
+          <View style={styles.buttonContainer2}>
+            <TouchableOpacity
+              style={styles.retakeButton}
+              onPress={() => setImageURI(null)}
+            >
+              <Text style={styles.retakeText}>Retake</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.retakeButton}
+              onPress={() => sendImageToServer(imageURI)}
+            >
+              <Text style={styles.confirmText}>Confirm</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <CameraView ref={cameraRef} style={styles.camera} ratio="16:9">
+          <View style={styles.pickerset}>
+            <Text style={styles.label}>사용자 선택</Text>
+            <Picker
+              selectedValue={selectedUser}
+              onValueChange={(itemValue) => setSelectedUser(itemValue)}
+              style={styles.picker}
+            >
+              {Array.isArray(users) &&
+                users.map((user, index) => (
+                  <Picker.Item
+                    key={index}
+                    label={user.userName}
+                    value={user.userName}
+                  />
                 ))}
-              </Picker>
-            </View>
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.captureButton} onPress={takeImage}>
-                <MaterialIcons name="camera" size={50} color="white" />
-              </TouchableOpacity>
-            </View>
-            <View>
-                <Text style={styles.instructions}>
-                  {leftfeet ? "오른쪽 발을 찍어주세요." : "왼쪽 발을 찍어주세요."}
-                </Text>
-            </View>
-          </CameraView>
-        )}
+              {/* {users.map((user, index) => (
+                <Picker.Item
+                  key={index}
+                  label={user.userName}
+                  value={user.userName}
+                />
+              ))} */}
+            </Picker>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.captureButton} onPress={takeImage}>
+              <MaterialIcons name="camera" size={50} color="white" />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={styles.instructions}>
+              {leftfeet ? "오른쪽 발을 찍어주세요." : "왼쪽 발을 찍어주세요."}
+            </Text>
+          </View>
+        </CameraView>
+      )}
     </View>
   );
 }
@@ -284,12 +313,12 @@ const styles = StyleSheet.create({
     color: "#333",
   },
   confirmButton: {
-    textAlign: 'center',
+    textAlign: "center",
     backgroundColor: "#404040",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
-    margin: '5%'
+    margin: "5%",
   },
   pickerset: {
     flexDirection: "row",
@@ -308,7 +337,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#d1d9e6",
     marginHorizontal: 30,
-    marginVertical: 10
+    marginVertical: 10,
   },
   picker: {
     flex: 1,
@@ -338,7 +367,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 0,
     position: "absolute",
     textAlign: "center",
-    bottom: 3
+    bottom: 3,
   },
   captureButton: {
     width: 70,
@@ -360,12 +389,12 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   retakeButton: {
-    textAlign: 'center',
+    textAlign: "center",
     backgroundColor: "#404040",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
-    margin: 3
+    margin: 3,
   },
   retakeText: {
     textAlign: "center",
@@ -377,13 +406,13 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   instructions: {
-    position: 'absolute',
+    position: "absolute",
     justifyContent: "center",
     alignSelf: "center",
     textAlign: "center",
     color: "green",
     bottom: 14,
-    fontSize: 20
+    fontSize: 20,
   },
   imageContainer: {
     flexDirection: "column",
@@ -397,23 +426,23 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 5,
     elevation: 3, // For Android shadow
-    width: '100%',
-    height: '100%',
-    paddingVertical: '40%'
+    width: "100%",
+    height: "100%",
+    paddingVertical: "40%",
   },
   middleContainerFirst: {
     gap: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    flexDirection: "row",
+    justifyContent: "space-evenly",
     margin: 0,
     width: "100%",
     height: "80%",
   },
   middleContainerSecond: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: '100%',
-    height: '40%',
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+    height: "40%",
     gap: 1,
   },
   imageText: {
@@ -428,7 +457,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 3,
     elevation: 2, // For Android shadow
-    width: '45%'
+    width: "45%",
   },
   imageTextTitle: {
     fontSize: 16,
